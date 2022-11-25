7c7d3f842743d9d46294c33eb6dc13118284fc53
jaseel <jaseeljctcs@gmail.com>
2022-11-25 17:16:23 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CourseAddComponent } from './course-add/course-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseAddComponent
   ],
   imports: [
     BrowserModule
