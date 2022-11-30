36a4708aeddc9f410c0735187e37517dc6e0da82
jaseel <jaseeljctcs@gmail.com>
2022-11-30 10:07:55 +0530

    api add our data

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -11,4 +11,7 @@ export class ApiService {
   {
     return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
   }
+  addcourse=(dataToSend:any)=>{
+    return this.http.post("http://mylinkurcodesapp.herokuapp.com/addcourse",dataToSend)
+  }
 }
--- src/app/course-add/course-add.component.ts
+++ src/app/course-add/course-add.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { createUrlTreeFromSnapshot } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-course-add',
@@ -12,14 +13,35 @@ export class CourseAddComponent {
   courseDuration=""
   courseDate=""
   courseVenue=""
+  constructor(private api:ApiService){}
+  readValue=()=>{
+ let data={
+  "courseTitle":this.courseTitle,"courseDescription":this.courseDescription,
+  "courseDate":this.courseDate,"courseDuration":this.courseDuration,
+  "courseVenue":this.courseVenue
+ }
+ console.log(data)
+ this.api.addcourse(data).subscribe(
+  (resp:any)=>{
+    console.log(resp)
+    if (resp.status=="success"){
 
-
-
-
-  viewList=()=>
-  {
-    let data:any={"courseTitle":this.courseTitle,"courseDescription": this.courseDescription,"courseDuration":this.courseDuration,
-    "courseDate":this.courseDate,"courseVenue":this.courseVenue}
-    console.log(data)
+      alert("course added succesfully")
+    }else{
+      alert("something went wrong")
+    }
   }
+ )
+
+  }
+
+
+
+
+  // viewList=()=>
+  // {
+  //   let data:any={"courseTitle":this.courseTitle,"courseDescription": this.courseDescription,"courseDuration":this.courseDuration,
+  //   "courseDate":this.courseDate,"courseVenue":this.courseVenue}
+   
+  // }
 }
